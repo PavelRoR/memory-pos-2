30601b1cc43cd04a09597b15d761147d65831dd0
PavelRoR <pavelvp2182@yandex.ru>
2021-10-23 13:55:43 +0500

    Main page is typeseted and stylezed

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,68 @@
 $(document).ready(function() {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this);
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('↙ Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
 
-
-
+                // button.text('Отправляем...');
+                // setTimeout(function () {
+                //     button.text('Отправлено!');
+                // }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#464343",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#464343",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+    // Модалка
+    $('.button-up').magnificPopup({
+        type: 'inline',
+        midClick: true, 
+        mainClass: 'mfp-fade'
+    });
+    /*Видео-отзывы*/
+    $(".video-wrapper-rev").click(function () {
+        $(".video-wrapper-rev iframe").each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв"><div class="yt-button"></div>');
+        })
+        var a = $(this).attr("data-youtube");
+        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1" allowfullscreen></iframe>')
+    });
 /*Конец документа*/
 });
